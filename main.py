bf2530545ba4687fd783fbd5ce9b731a489fb704
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2022-07-08 17:43:26 +0400

    created list of all directions in FEFU

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests,json
-your_id = input('Снилс')
-
+#your_id = input('Снилс')
+your_id = "143-270-627 37"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
@@ -12,45 +12,32 @@ params = {
     'action': 'getStudents',
 }
 
+from directions_list import directions_list
+print(directions_list)
+
+
 data = {
     'admissionCampaignType': 'Прием на обучение на бакалавриат/специалитет',
     'financingSource': 'Бюджетная основа',
     'studyForm': 'Очная',
     'implementationPlace': 'Владивосток',
-    'trainingDirection': '09.03.03 Прикладная информатика',
-    'consent': 'false',
-}
-data2 = {
-    'admissionCampaignType': 'Прием на обучение на бакалавриат/специалитет',
-    'financingSource': 'Бюджетная основа',
-    'studyForm': 'Очная',
-    'implementationPlace': 'Владивосток',
-    'trainingDirection': '09.03.04 Программная инженерия',
-    'consent': 'false',
-}
-data3 = {
-    'admissionCampaignType': 'Прием на обучение на бакалавриат/специалитет',
-    'financingSource': 'Бюджетная основа',
-    'studyForm': 'Очная',
-    'implementationPlace': 'Владивосток',
-    'trainingDirection': '10.03.01 Информационная безопасность',
+    'trainingDirection': '10.05.01 Компьютерная безопасность',
     'consent': 'false',
 }
 
-spisok = [data,data2,data3]
+
+spisok = [data]
 for i in spisok:
     dir = ''
     if i == data:
-        dir = 'Прикладная информатика'
-    elif i == data2:
-        dir =  'Программная инженерия'
-    elif i == data3:
-        dir = 'Информационная безопасность'
+        dir = 'Компьютерная безопасность'
     response = requests.post('https://www.dvfu.ru/bitrix/services/main/ajax.php', params=params, headers=headers, data=i).json()
     order = ''
     data = response.get('data')
     for id in data:
         if your_id == id['name']:
             order = int(float((id.get('GENERALORDER'))))
+            
             break
-    print(f'{dir}: {order:.0f}')
+    print(dir, order)
+    #print(f'{dir}: {order:.0f}')
