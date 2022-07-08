dbd566c88aef4c90c34d2dda57e8abe2df53714f
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2022-07-08 18:41:30 +0400

    added printing current state in direction and number of participants of direction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests,json
+from directions_list import directions_list
+
 #your_id = input('Снилс')
-your_id = "143-270-627 37"
+your_id = "166-275-946 05"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
@@ -12,26 +14,27 @@ params = {
     'action': 'getStudents',
 }
 
-from directions_list import directions_list
-print(directions_list)
 
 
-data = {
+
+
+base_data = {
     'admissionCampaignType': 'Прием на обучение на бакалавриат/специалитет',
     'financingSource': 'Бюджетная основа',
     'studyForm': 'Очная',
     'implementationPlace': 'Владивосток',
-    'trainingDirection': '10.05.01 Компьютерная безопасность',
+    'trainingDirection': None,
     'consent': 'false',
 }
 
 
-spisok = [data]
-for i in spisok:
-    dir = ''
-    if i == data:
-        dir = 'Компьютерная безопасность'
-    response = requests.post('https://www.dvfu.ru/bitrix/services/main/ajax.php', params=params, headers=headers, data=i).json()
+print('"Направление" место/всего участников')
+result = {}
+for direction in directions_list:
+    current_data = base_data
+    current_data['trainingDirection'] = direction
+
+    response = requests.post('https://www.dvfu.ru/bitrix/services/main/ajax.php', params=params, headers=headers, data=current_data).json()
     order = ''
     data = response.get('data')
     for id in data:
@@ -39,5 +42,13 @@ for i in spisok:
             order = int(float((id.get('GENERALORDER'))))
             
             break
-    print(dir, order)
+    max_order =  int(float((data[-1].get('GENERALORDER'))))
+
+    if order:
+        result[direction] = order
+        print(f'"{direction}" {order}/{max_order}')
+        if len(result) == 3:
+            break
     #print(f'{dir}: {order:.0f}')
+
+
